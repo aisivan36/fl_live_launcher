f4fccc6b174b3cd3148a6868959b81f4f01a0e70
ivan-boginski <aisivan36@gmail.com>
2022-02-07 08:44:55 +0700

    To show all the apps that only have a Launcher Icon

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -1,5 +1,9 @@
 import 'package:device_apps/device_apps.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final appsProvider = FutureProvider<List<Application>>((ref) async =>
-    await DeviceApps.getInstalledApplications(includeAppIcons: true));
+final appsProvider = FutureProvider<List<Application>>(
+    (ref) async => await DeviceApps.getInstalledApplications(
+          includeAppIcons: true,
+          includeSystemApps: true,
+          onlyAppsWithLaunchIntent: true,
+        ));
